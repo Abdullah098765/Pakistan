0befccfed328fc7e3d669e486a8f7628bc3c0402
Abdullah <ssmmhazz@gmail.com>
2022-09-06 22:20:15 +0500

    'Pak'

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
-import express from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
 import cors from 'cors'
 
-import postRouter from "./routes/posts.js"; 
+import postRouter from './routes/posts.js'
 
-const app = express();
+const app = express()
 
-app.use(bodyParser.json({ limit: "30mb", extanded: true }))
-app.use(cors());
+app.use(bodyParser.json({ limit: '30mb', extanded: true }))
+app.use(cors())
 
 app.use('/', postRouter)
 
-
-
-
-
-
-const CONNECTION_URL = "mongodb+srv://testdb:mern123@testdb.nuqjg.mongodb.net/?retryWrites=true&w=majority"
-const PORT = process.env.PORT || 5001;
+const CONNECTION_URL =  'mongodb+srv://testdb:mern123@testdb.nuqjg.mongodb.net/?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5001
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopoLogy: true })
-    .then((e) => { app.listen(PORT, (a) => console.log(`Server running on port: ${PORT}`)) })
-    .catch((err) => { console.log(err) });
+  .then(e => {
+    console.log('Connected to MDB')
+  })
+  .catch(err => {
+    console.log(err)
+  })
 
-
+app.listen(PORT, a => console.log(`Server running on port: ${PORT}`))
